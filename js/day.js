724f309cf78a6fabcf100f5a1bb026cf8469b9f1
A4rlLab <a4rllab@163.com>
2023-03-18 12:48:39 +0800

    Site updated: 2023-03-18 12:48:35

---
+++ js/day.js
@@ -0,0 +1,120 @@
+var d = new Date();
+m = d.getMonth() + 1;
+dd = d.getDate();
+y = d.getFullYear();
+
+// 公祭日
+if (m == 9 && dd == 18) {
+    document.getElementsByTagName("html")[0].setAttribute("style", "filter: grayscale(60%);");
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("今天是九一八事变" + (y - 1931).toString() + "周年纪念日\n勿忘国耻，但也不要忘了用国际主义原则去斗争！");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+if (m == 7 && dd == 7) {
+    document.getElementsByTagName("html")[0].setAttribute("style", "filter: grayscale(60%);");
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("今天是卢沟桥事变" + (y - 1937).toString() + "周年纪念日\n勿忘国耻，但也不要忘了用国际主义原则去斗争！");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+if (m == 12 && dd == 13) {
+    document.getElementsByTagName("html")[0].setAttribute("style", "filter: grayscale(60%);");
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("今天是南京大屠杀" + (y - 1937).toString() + "周年纪念日\n勿忘国耻，但也不要忘了用国际主义原则去斗争！");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+if (m == 8 && dd == 14) {
+    document.getElementsByTagName("html")[0].setAttribute("style", "filter: grayscale(60%);");
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("今天是世界慰安妇纪念日\n勿忘国耻，但也不要忘了用国际主义原则去斗争！");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+if (m == 9 && dd == 9) {
+    document.getElementsByTagName("html")[0].setAttribute("style", "filter: grayscale(60%);");
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("今天是伟大领袖毛主席逝世" + (y - 1976).toString() + "周年纪念日\n“你们不要怀念我，我走以后，你们就是我！”");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+
+
+// 节假日
+
+if (m == 5 && dd == 1) {//劳动节
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("劳动节快乐\n向伟大的无产阶级致敬！");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+if (m == 5 && dd == 4) {//青年节
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("青年节快乐\n“你们是早上八九点钟的太阳”，“世界是你们的”！");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+//传统节日部分
+
+if ((y == 2023 && m == 4 && dd == 5) || (y == 2024 && m == 4 && dd == 4) || (y == 2025 && m == 4 && dd == 4)) {//清明节
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("清明时节雨纷纷,一束鲜花祭故人。");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+
+var lunar = calendarFormatter.solar2lunar();
+
+//农历采用汉字计算，防止出现闰月导致问题
+
+if ((lunar["IMonthCn"] == "正月" && lunar["IDayCn"] == "初六") || (lunar["IMonthCn"] == "正月" && lunar["IDayCn"] == "初五") || (lunar["IMonthCn"] == "正月" && lunar["IDayCn"] == "初四") || (lunar["IMonthCn"] == "正月" && lunar["IDayCn"] == "初三") || (lunar["IMonthCn"] == "正月" && lunar["IDayCn"] == "初二") || (lunar["IMonthCn"] == "正月" && lunar["IDayCn"] == "初一") || (lunar["IMonthCn"] == "腊月" && lunar["IDayCn"] == "三十") || (lunar["IMonthCn"] == "腊月" && lunar["IDayCn"] == "廿九")) {
+    //春节，本来只有大年三十到初六，但是有时候除夕是大年二十九，所以也加上了
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire(y.toString() + "年新年快乐\n日复一日，年复一年，念念不忘，必有回响。");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+if ((lunar["IMonthCn"] == "正月" && lunar["IDayCn"] == "十五")) {
+    //元宵节
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("元宵节快乐\n元宵，愿晓。");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+if ((lunar["IMonthCn"] == "五月" && lunar["IDayCn"] == "初五")) {
+    //端午节
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("端午节快乐\n日出东山，枪指西天。");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+if ((lunar["IMonthCn"] == "七月" && lunar["IDayCn"] == "初七")) {
+    //七夕节
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("七夕节快乐\n“人与人之间的爱，是多么单纯。”");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+if ((lunar["IMonthCn"] == "八月" && lunar["IDayCn"] == "十五")) {
+    //中秋节
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("中秋节快乐\n举头月光白，低眉红旗卷。");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+if ((lunar["IMonthCn"] == "九月" && lunar["IDayCn"] == "初九")) {
+    //重阳节
+    if (sessionStorage.getItem("isPopupWindow") != "1") {
+        Swal.fire("重阳节快乐\n独在异乡为异客，每逢佳节倍思亲。");
+        sessionStorage.setItem("isPopupWindow", "1");
+    }
+}
+
+// 切换主题提醒
+// if (y == 2022 && m == 12 && (dd >= 18 && dd <= 20)) {
+//     if (sessionStorage.getItem("isPopupWindow") != "1") {
+//         Swal.fire("网站换成冬日限定主题啦⛄");
+//         sessionStorage.setItem("isPopupWindow", "1");
+//     }
+// }
